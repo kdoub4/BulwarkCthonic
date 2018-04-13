f4c6123c23a0169b3dc39f64a0de20712e233e10
Kevin Worth <githubh@truesteel.fastmail.fm>
2018-04-13 16:20:25 -0600

    cardtype

---
+++ vDom/src/main/java/com/vdom/core/CardType.java
@@ -0,0 +1,16 @@
+package com.vdom.core;
+
+public enum CardType {
+	Action, Treasure, Victory, Curse, Spell,
+	Attack, InHandReaction, Technique, Defend, OnAttack, InHandManoeuvre, AttackStepEnd, OnActivation,
+	OnTrash,
+	Heroism, Wound, Hero,
+	Location, Equipment, Fortification, RemainsManoeuvre, RemainsReaction, Defenders,
+	Enemy, WhenDrawn, Activate, Horseman, Undead, Necromancer,
+    Duration,
+	Prize,
+	Shelter, Range, Blast, Knight,
+	Remains, AnyTime, Crown,
+	Gathering, Castle,
+	Event, Landmark
+}
